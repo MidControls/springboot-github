d7f5f225c2f911193dd4ea018fce60db7c2f53f4
gakki <mid_control@outlook.com>
2018-12-01 12:03:17 +0800

    first

--- src/main/java/com/gakki/smile/controller/TestController.java
+++ src/main/java/com/gakki/smile/controller/TestController.java
@@ -5,13 +5,19 @@ import com.gakki.smile.bean.People;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.Arrays;
+
+
 @RestController()
 public class TestController {
 
 
     @GetMapping(value = "/hello")
     public Object test(){
-        return new People("wsj",20);
+
+      return Arrays.asList(new People("wsj",20),new People("pqy",30));
+
     }
 
 }
